0821fb7f7ee9aac9721ae5eb8d673e6a9a3f0220
tsfong <tsfong@uci.edu>
2021-12-13 15:45:01 -0800

    add SignIn user view

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views.users import SignUp
+from .views.users import SignUp, SignIn
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
+    path('sign-in/', SignIn.as_view(), name='sign-in'),
 ]
--- api/views/users.py
+++ api/views/users.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import status, generics
+from rest_framework.authtoken.models import Token
 from ..serializers.user import UserSerializer
+from django.contrib.auth import authenticate, login
 
 class SignUp(generics.CreateAPIView):
     # Override the authentication/permissions classes so this endpoint
@@ -18,3 +20,32 @@ class SignUp(generics.CreateAPIView):
             return Response({ 'user': created_user.data }, status=status.HTTP_201_CREATED)
         else:
             return Response(created_user.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SignIn(generics.CreateAPIView):
+    # Override the authentication/permissions classes so this endpoint
+    # is not authenticated & we don't need any permissions to access it.
+    authentication_classes = ()
+    permission_classes = ()
+
+    def post(self, request):
+        data = request.data['user']
+        # use django authenticate to verify password and email match
+        user = authenticate(request, email=data['email'], password=data['password'])
+        # Is our user is successfully authenticated...
+        if user is not None:  
+            login(request, user)
+            # use django generate a token and save it to user
+            Token.objects.filter(user=user).delete()
+            token = Token.objects.create(user=user)
+            user.token = token.key
+            user.save()
+            # return the user with their id, email and token
+            return Response({
+                'user': {
+                    'id': user.id,
+                    'email': user.email,
+                    'token': token.key
+                }
+            })
+        else:
+            return Response({ 'msg': 'The username and/or password is incorrect.' }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
