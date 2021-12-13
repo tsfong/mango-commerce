0ba348ec920c5b1ed3f48b81ce4d81fd07f435f5
tsfong <tsfong@uci.edu>
2021-12-13 14:58:35 -0800

    add user serializer for sign up route

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -1,1 +1,13 @@
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
+# from ..models.user import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'password')
+        extra_kwargs = { 'password': { 'write_only': True, 'min_length': 5 } }
+    
+        # This create method will be used for model creation
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
--- api/views/users.py
+++ api/views/users.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status, generics
+from ..serializers.user import UserSerializer
 
 class SignUp(generics.CreateAPIView):
     # Override the authentication/permissions classes so this endpoint
@@ -8,5 +9,12 @@ class SignUp(generics.CreateAPIView):
     permission_classes = ()
     
     def post(self, request):
-         print('wahooo')
-         return Response({})
+        # Create the user using the UserSerializer 
+        created_user = UserSerializer(data=request.data['user'])
+        # Check user is valid
+        if created_user.is_valid():
+            # Save the user and send back a response!
+            created_user.save()
+            return Response({ 'user': created_user.data }, status=status.HTTP_201_CREATED)
+        else:
+            return Response(created_user.errors, status=status.HTTP_400_BAD_REQUEST)
